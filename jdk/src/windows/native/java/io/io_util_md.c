bebc248cb58bb152055582888fa8f97a851fa647
Peter Shipton <Peter_Shipton@ca.ibm.com>
2025-01-08 19:49:43 -0500

    Trc_io_handleOpen_Entry

--- jdk/src/windows/native/java/io/io_util_md.c
+++ jdk/src/windows/native/java/io/io_util_md.c
@@ -267,7 +267,7 @@ winFileHandleOpen(JNIEnv *env, jstring path, int flags)
 	    if (NULL != pathStr) {
 	        WideCharToMultiByte(CP_UTF8, 0, pathbuf, -1, pathStr, length, NULL, NULL);
 	    }
-	    Trc_io_CreateFileW_Entry(pathStr, access, sharing, disposition, flagsAndAttributes);
+	    Trc_io_handleOpen_Entry(pathStr, access, sharing, disposition, flagsAndAttributes);
 	    free(pathStr);
 	}
 
